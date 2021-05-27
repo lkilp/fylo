8981dac071d0fc88202f3417ae73d44aa02b96fc
liis.kilp <kilp.liis@gmail.com>
2021-05-27 09:12:23 +0300

    git init and cleanup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import Attribution from './components/Attribution';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import Intro from './components/Intro';
+import StayProductive from './components/StayProductive';
+import TestimonialsContainer from './components/TestimonialsContainer';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Intro />
+      <StayProductive />
+      <TestimonialsContainer />
+      <Footer />
+      <Attribution />
     </div>
   );
 }
